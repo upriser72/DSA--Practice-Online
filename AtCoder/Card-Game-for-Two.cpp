2c909064e39048d0163c2102b22523232b50a21b
Vanshika Srivastava <134536521+upriser72@users.noreply.github.com>
2024-02-25 20:27:00 +0530

    Update Card-Game-for-Two.cpp
    walmart

--- AtCoder/Card-Game-for-Two.cpp
+++ AtCoder/Card-Game-for-Two.cpp
@@ -1,3 +1,15 @@
+/*
+We have N cards. A number ai is written on the i-th card.
+Alice and Bob will play a game using these cards. In this game, Alice and Bob alternately take one card. Alice goes first.
+The game ends when all the cards are taken by the two players, and the score of each player is the sum of the numbers written on the cards he/she has taken. 
+When both players take the optimal strategy to maximize their scores, find Alice's score minus Bob's score.
+
+Constraints:
+N is an integer between 1 and 100 (inclusive).
+ai(1≤i≤N) is an integer between 1 and 100 (inclusive).
+*/
+
+
 #include<bits/stdc++.h>
 using namespace std;
 int main()
